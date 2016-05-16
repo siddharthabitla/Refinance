618ec741d8dde735de2ae9283abc26222ade2130
siddharthabitla <bitlasiddhartha@gmail.com>
2016-05-16 22:50:11 +0530

    Added a simple output change.

--- Abstract/src/demo/Area.java
+++ Abstract/src/demo/Area.java
@@ -4,6 +4,7 @@ public abstract class Area {
   void newline() {
     System.out.println();
     System.out.println("+------------------+------------------+");
+    System.out.println("+------------------+------------------+");
     System.out.println();
   }
   abstract void welcome();
