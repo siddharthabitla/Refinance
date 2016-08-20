9be852f35d79907f8743213c43b5e466fce7e502
siddharthabitla <bitlasiddhartha@gmail.com>
2016-08-20 10:02:51 +0530

    Implemented UserDao.

--- Refinance/src/com/slokam/dao/UserDao.java
+++ Refinance/src/com/slokam/dao/UserDao.java
@@ -1,27 +1,41 @@
 package com.slokam.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.slokam.Entity.UserPojo;
 
 @Repository
-public class UserDao {
+public class UserDao implements UserDaoInterface {
 	@Autowired
 	private HibernateTemplate ht;
-	
-	public void saveUser(UserPojo user) {
+
+	public void addUser(UserPojo user) {
 		ht.save(user);
 	}
-	public boolean getUser(UserPojo userPojo) {
-		List<UserPojo> list =
-				(List<UserPojo>) ht.find("from UserPojo where id=? and ssn=?", userPojo.getId(), userPojo.getSsn());
-		if (list.size() > 0)
-			return true;
-		else
-			return false;
+
+	@SuppressWarnings("unchecked")
+	public List<UserPojo> getAllUsers() {
+		return ht.executeFind(new HibernateCallback<List<UserPojo>>() {
+			public List<UserPojo> doInHibernate(Session session) throws HibernateException, SQLException {
+				return session.createQuery("from UserPojo u").list();
+			}
+		});
 	}
+
+	public void deleteUser(int id) {
+		UserPojo user = (UserPojo) ht.find("from UserPojo where id=" + id);
+		if (user != null)
+			ht.delete(user);
+		else
+			System.out.println("Requested User is not in our database.");
+	}
+
 }  // UserDao
